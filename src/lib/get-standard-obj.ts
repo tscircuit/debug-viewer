c4bb16101bcfd7a0df3fba0210bc34efd9b0c147
Severin Ibarluzea <seveibar@gmail.com>
2023-07-22 16:03:42 -0700

    fix: colors sometimes not being found, fallback color

--- src/lib/get-standard-obj.ts
+++ src/lib/get-standard-obj.ts
@@ -3,11 +3,12 @@ import colors from "nice-color-palettes"
 import { U } from "ts-toolbelt"
 
 function stringHash(str: string) {
+  str = str.toString()
   let hash = 0
   if (str.length == 0) return hash
   for (var i = 0; i < str.length; i++) {
     var char = str.charCodeAt(i)
-    hash = (hash << 5) - hash + char
+    hash = Math.abs((hash << 5) - hash + char)
     hash = hash & hash // Convert to 32bit integer
   }
   return hash
@@ -64,6 +65,8 @@ export const getStandardObj = (
     height,
     title,
     content,
-    bg_color: colors[stringHash((lo as any).type || title) % colors.length][4],
+    bg_color:
+      colors[stringHash((lo as any).type || title) % colors.length]?.[4] ??
+      "#f00",
   }
 }
